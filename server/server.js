d9c665a4ee2abc05bf4d8d093d66dc88306213dc
S M <sm@ss-macbook-pro.lan>
2022-08-03 10:14:37 -0400

    added some testing

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ const cors = require('cors');
 //import routers here
 //router to get docker data
 
-
 //handle parsing request body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,7 +16,6 @@ app.use(cors());
 
 //serve static files
 app.use(express.static(path.resolve(__dirname, '../dist')));
-
 
 //to avoid axios CORS errors IN development
 // app.use(function (req, res, next) {
@@ -32,29 +30,25 @@ app.use(express.static(path.resolve(__dirname, '../dist')));
 //   next();
 // });
 
-
-
 //catch-all route handler for any requests to an unknown route
 app.get('*', (req, res) => {
-    return res.status(404).send('This is an unknown URL.');
-  })
+  return res.status(404).send('This is an unknown URL.');
+});
 
-  
 //global error handlings
 app.use((err, req, res, next) => {
-    const defaultErr = {
-      log: 'Express error handler caught an unknown middleware error',
-      status: 500,
-      message: { err: 'An error occurred' },
-    };
-    const errorObj = Object.assign(defaultErr, err);
-    console.log(errorObj.log);
-    res.status(errorObj.status).json(errorObj.message);
-  });
+  const defaultErr = {
+    log: 'Express error handler caught an unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign(defaultErr, err);
+  console.log(errorObj.log);
+  res.status(errorObj.status).json(errorObj.message);
+});
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}`);
-  });
+  console.log(`Server listening on port: ${PORT}`);
+});
 
-
-module.exports =  {app};
+module.exports = { app };
