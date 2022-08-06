9c00c677eed3a8290b8dd586d9d14599ca985346
S M <sm@ss-macbook-pro.lan>
2022-08-06 15:31:08 -0400

    updated button and styles

--- src/components/quitButton.tsx
+++ src/components/quitButton.tsx
@@ -10,13 +10,8 @@ export default function QuitButton() {
         onClick={() => {
           dispatch(quitButton());
         }}
-      />
-      <button className='cta'>
-        <span>Log State</span>
-        <svg viewBox='0 0 13 10' height='10px' width='15px'>
-          <path d='M1,5 L11,5'></path>
-          <polyline points='8 1 12 5 8 9'></polyline>
-        </svg>
+      >
+        Log State
       </button>
     </div>
   );
