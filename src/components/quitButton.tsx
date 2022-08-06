a7eb46a76496109bcc0e09c500cbaa0addb4f25e
S M <sm@ss-macbook-pro.lan>
2022-08-06 15:21:11 -0400

    frontend week1 updates

--- src/components/quitButton.tsx
+++ src/components/quitButton.tsx
@@ -2,22 +2,6 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { quitButton } from '../reducers/containerStatusSlice';
 
-// let dispatch = useDispatch();
-
-// const QuitButton: React.FC = () => (
-//   <div className='header-component'>
-//     <button
-//       onClick={() => {
-//         dispatch(quitButton);
-//       }}
-//     >
-//       Quit
-//     </button>
-//   </div>
-// );
-
-// export default QuitButton;
-
 export default function QuitButton() {
   const dispatch = useDispatch();
   return (
@@ -26,50 +10,14 @@ export default function QuitButton() {
         onClick={() => {
           dispatch(quitButton());
         }}
-      >
-        Get State!!!
+      />
+      <button className='cta'>
+        <span>Log State</span>
+        <svg viewBox='0 0 13 10' height='10px' width='15px'>
+          <path d='M1,5 L11,5'></path>
+          <polyline points='8 1 12 5 8 9'></polyline>
+        </svg>
       </button>
     </div>
   );
 }
-
-////store
-// import { configureStore } from '@reduxjs/toolkit'
-
-// export const store = configureStore({
-//   reducer: {},
-// })
-
-// // Infer the `RootState` and `AppDispatch` types from the store itself
-// export type RootState = ReturnType<typeof store.getState>
-// // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-// export type AppDispatch = typeof store.dispatch
-
-// import React from 'react'
-// import type { RootState } from '../../app/store'
-// import { useSelector, useDispatch } from 'react-redux'
-// import { decrement, increment } from './counterSlice'
-
-// export function Counter() {
-//   const count = useSelector((state: RootState) => state.counter.value)
-//   const dispatch = useDispatch()
-
-//   return (
-//     <div>
-//       <div>
-//         <button
-//           aria-label="Increment value"
-//           onClick={() => dispatch(increment())}
-//         >
-//           Increment
-//         </button>
-//         <span>{count}</span>
-//         <button
-//           aria-label="Decrement value"
-//           onClick={() => dispatch(decrement())}
-//         >
-//           Decrement
-//         </button>
-//       </div>
-//     </div>
-//   )
